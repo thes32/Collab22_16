2a30c710bb844bce48120e632a0cba9399345a92
Serban Vornicu <serban.vornicu12@yahoo.co.uk>
2022-01-16 14:42:11 +0000

    Getting rid of the old proj

--- Proto2/Source/Proto2/Proto2.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Proto2.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proto2, "Proto2" );
